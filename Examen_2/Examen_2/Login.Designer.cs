f7ed0ab3059125d9cf429e20e8880085b562b13f
Oscarlira1 <48262381+Oscarlira1@users.noreply.github.com>
2022-11-06 03:00:48 -0600

    Add files via upload

---
+++ Examen_2/Examen_2/Login.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Examen_2
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Contra = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Contra
+            // 
+            this.Contra.BackColor = System.Drawing.Color.Wheat;
+            this.Contra.Location = new System.Drawing.Point(316, 104);
+            this.Contra.Name = "Contra";
+            this.Contra.Size = new System.Drawing.Size(155, 20);
+            this.Contra.TabIndex = 0;
+            // 
+            // Usuario
+            // 
+            this.Usuario.BackColor = System.Drawing.Color.Wheat;
+            this.Usuario.Location = new System.Drawing.Point(75, 104);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(155, 20);
+            this.Usuario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(132, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(237, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Iniciar Sección";
+            // 
+            // Salir
+            // 
+            this.Salir.BackColor = System.Drawing.Color.Gainsboro;
+            this.Salir.Location = new System.Drawing.Point(316, 185);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(109, 46);
+            this.Salir.TabIndex = 7;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = false;
+            // 
+            // Conectar
+            // 
+            this.Conectar.BackColor = System.Drawing.Color.Gainsboro;
+            this.Conectar.Location = new System.Drawing.Point(122, 185);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(108, 46);
+            this.Conectar.TabIndex = 8;
+            this.Conectar.Text = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = false;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImage = global::Examen_2.Properties.Resources.IMG_4252;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(567, 283);
+            this.Controls.Add(this.Conectar);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.Contra);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Contra;
+        private System.Windows.Forms.TextBox Usuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Examen_2/Examen_2/Login.cs
@@ -0,0 +1,59 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Conectar_Click(object sender, EventArgs e)
+        {
+            if (Usuario.Text == string.Empty)
+            {
+                errorProvider1.SetError(Usuario, "Ingrese el usuario");
+                Usuario.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (Contra.Text == string.Empty)
+            {
+                errorProvider1.SetError(Contra, "Ingrese la clave");
+                Usuario.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            Usuario_Datos Colesterol = new Usuario_Datos();
+
+            bool valido = await(Colesterol.LoginAsync(Usuario.Text, Contra.Text));
+
+            if (valido)
+            {
+                //mostrar si puede acceder 
+                Menu_de_opciones menu = new Menu_de_opciones();
+                Hide();
+                menu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Datos de usuario incorrecto ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
